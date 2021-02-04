c5c4f3b80feffbc4280f332641cd2d99c054c4f5
Orilym <florian.millet@gmail.com>
2021-02-04 19:34:29 +0100

    fuck you mother fucker
    just le composant film pour lister les trendings a refaire

--- NetflixProject/src/app/app.module.ts
+++ NetflixProject/src/app/app.module.ts
@@ -4,14 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FilmComponent } from './components/film/film.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    FilmComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
